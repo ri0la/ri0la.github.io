4ad1aa26396d2611d5122ba92f942e05b1bec8d3
ri0la <ram9828@nyu.edu>
2022-11-21 08:43:02 -0500

    chnages

---
+++ week11/themedium.js
@@ -0,0 +1,15 @@
+
+
+const playButton = document.querySelector('.butt1');
+const pauseButton = document.querySelector('.butt2');
+
+
+var vid = document.getElementById("heart");
+
+function playVid() {
+    vid.play();
+}
+
+function pauseVid() {
+    vid.pause();
+}
