21694805787a1c18a88b8567e928e6ac24af0db8
ri0la <ram9828@nyu.edu>
2022-12-03 23:33:42 -0500

    hiiii

--- week11/themedium.js
+++ week11/themedium.js
@@ -1,15 +1,17 @@
 
+var myVideo = document.getElementById("heart");
+function playVideo() { 
+ myVideo.play(); 
+} 
+function pauseVideo() { 
+ myVideo.pause(); 
+} 
 
-const playButton = document.querySelector('.butt1');
-const pauseButton = document.querySelector('.butt2');
 
-
-var vid = document.getElementById("heart");
-
-function playVid() {
-    vid.play();
-}
-
-function pauseVid() {
-    vid.pause();
-}
+var myVideo = document.getElementById("lights");
+function playVideo() { 
+ myVideo.play(); 
+} 
+function pauseVideo() { 
+ myVideo.pause(); 
+} 
