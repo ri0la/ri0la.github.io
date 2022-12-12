23f6c1bbb630303daeeb6be1ed6300edcf4f5b5d
ri0la <ram9828@nyu.edu>
2022-12-12 09:01:21 -0500

    music

--- Final/medium.js
+++ Final/medium.js
@@ -18,6 +18,34 @@ const out1 = document.getElementById('output1');
         out1.innerHTML = txt1.value;
   }
   submitbtn.addEventListener('click',orbit);
+
+audioPlayer();
+function audioPlayer(){
+  let currentSong = 0;
+  $("#audioPlayer")[0].src = $("#playlist li a")[0];
+  $("audioPlayer")[0].play();
+  $("playlist li a").click(function(e){
+      e.preventDefault();
+      $("#audioPlayer")[0].src = this;
+      $("#audioPlayer")[0].play();
+      $("#playlist li").removeClass(current-song);
+      currentSong = $(this).parent().index();
+      $(this).parent.addClass("current-song");
+  });
+/* https://youtu.be/vtZCMTtP-0Y */
+  $("#audioPlayer")[0].addEventListener("ended",function(){
+    currentSong++;
+      if(currentSong == $("#playlist li a").length)
+        currentSong = 0;
+      $("#playlist li").removeClass("current-song");
+      $("#playlist li:eq("+currentSong+")").addClass("current-song");
+      $("#audioPlayer")[0].src = $("#playlist li a")[currentSong].href;
+      $("audioPlayer")[0].play();
+  });
+}
+
+
+
 /*
 i tried to get the date and time show in the output when the submit button was pressed but i couldn't get it to work
 https://www.howtocodeschool.com/2020/05/display-current-date-and-time-using-html-javascript.html
